feb281c021db5055c30f383f8384986d3e2b369a
wigitalgreenstudio <WigitalGreenStudio>
2018-08-17 18:46:25 +0200

    Utworzenie glownego szablonu strony WWW

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="pl">
+	<head>
+		<meta charset="utf-8" />
+		<title>WigitalGreenStudio - Strony, Sklepy internetowe</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta http-equiv="X-UA-Compatible" content="ie=edge, chrome=1" />
+	</head>
+
+	<body>
+
+	</body>
+</html>
